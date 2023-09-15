15d4ea8b37011d880a72aa89324a3b1804cb64eb
raymond <raymondshawn779@gmail.com>
2023-09-15 09:55:54 -0600

    accepting pdf and also docs  file

--- app.py
+++ app.py
@@ -4,16 +4,57 @@ from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import CharacterTextSplitter
 import os
 from langchain.chains.summarize import load_summarize_chain
+from PyPDF2 import PdfReader
+from docx import Document
 
 
-def read_file_as_string(file):
+def read_file(file):
+    """Read file and return contents as a string"""
 
-    # To convert to a string based IO:
+    file_ext = get_file_ext(file)
+
+    if file_ext == "pdf":
+        return read_pdf(file)
+    elif file_ext == "docx":
+        return read_docx(file)
+    elif file_ext=="txt":
+        return read_txt(file)
+
+
+def get_file_ext(file):
+    """Get file extension from filename"""
+    return file.name.split(".")[-1]
+
+
+def read_pdf(file):
+    """Read PDF file and return text contents"""
+    text = ""
+    pdf = PdfReader(file)
+    num_pages = len(pdf.pages)
+
+    for i in range(num_pages):
+        page = pdf.pages[i]
+        content = page.extract_text()
+        text += content
+
+    return text
+
+
+def read_docx(file):
+    """Read DOCX file and return text contents"""
+    document = Document(file)
+
+    full_text = []
+    for para in document.paragraphs:
+        full_text.append(para.text)
+
+    return '\n'.join(full_text)
+
+
+def read_txt(file):
+    """Read file of other format and return text contents"""
     stringio = StringIO(file.getvalue().decode("utf-8"))
-
-    # To read file as string:
-    string_data = stringio.read()
-    return string_data
+    return stringio.read()
 
 
 def create_documents(text):
@@ -25,7 +66,6 @@ def create_documents(text):
     return documents
 
 
-
 def generate_summary(docs):
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
     chain = load_summarize_chain(llm, chain_type="stuff")
@@ -34,10 +74,10 @@ def generate_summary(docs):
 
 
 st.title("🦜️🔗 Summarize  File")
-st.text("Upload txt file")
-uploaded_files = st.file_uploader("Choose a  txt file",accept_multiple_files=True)
+st.text("Upload txt or docx or pdf file.You can upload multiple files")
+uploaded_files = st.file_uploader("Choose a  txt file", accept_multiple_files=True)
 
-with  st.form("Form", clear_on_submit=True):
+with  st.form("Form"):
     api_key = st.text_area("Enter your OpenAI key:")
 
     submitted = st.form_submit_button("Submit")
@@ -46,9 +86,7 @@ with  st.form("Form", clear_on_submit=True):
         with st.spinner('Generating Summary...'):
 
             for file in uploaded_files:
-                st.write(file.name)
-
-                text = read_file_as_string(file)
+                text = read_file(file)
                 documents = create_documents(text)
                 response = generate_summary(documents)
 
